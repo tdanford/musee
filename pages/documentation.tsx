43304feff27fccc53449ab2f751f6f0ef658fa0a
Timothy Danford <tdanford@gmail.com>
2022-03-10 09:28:13 -0500

    Cleanup imports

--- pages/documentation.tsx
+++ pages/documentation.tsx
@@ -1,5 +1,4 @@
-import { setDefaultResultOrder } from "dns";
-import React, { useState, useRef, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import MuseeContent, { MuseeContentType } from "../components/MuseeContent";
 import MuseeStep from "../components/MuseeStep";
 
