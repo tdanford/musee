d9edad09b895f93e7ecfb91e040446866c3418d8
Timothy Danford <tdanford@gmail.com>
2022-03-10 09:00:03 -0500

    documentation.tsx loads the config JSON (now called the 'art') dynamically from a URL

--- pages/documentation.tsx
+++ pages/documentation.tsx
@@ -1,10 +1,9 @@
+import { setDefaultResultOrder } from "dns";
 import React, { useState, useRef, useEffect } from "react"
 import MuseeContent, { MuseeContentType } from "../components/MuseeContent";
 import MuseeStep from "../components/MuseeStep";
 
 import styles from '../styles/Musee.module.css'
-
-import example from './example.json' 
 
 type MuseeStepType = {
     instruction: string, 
@@ -15,9 +14,30 @@ export type DocumentationType = {
     steps: MuseeStepType[]
 }
 
+function Error({ reason }: { reason: any}) {
+    return <div style={{color: "red", backgroundColor: "white"}}>
+        {reason}
+    </div>
+}
+
 function Musee() {
     const [currentStep, setCurrentStep] = useState(0); 
-    const docs: DocumentationType = example as DocumentationType;
+    const [art, setArt] = useState<DocumentationType>({ steps: [] })
+    const [error, setError] = useState<any>(null);
+
+    useEffect(() => {
+        fetch("/arts/example.json", {
+            method: "GET", 
+            headers: {
+                Accept: "application/json"
+            }
+        }).then(response => response.json())
+        .then((loaded: DocumentationType) => { 
+            setArt(loaded)
+            setError(null)
+        })
+        .catch((reason: any) => setError(reason))
+    }, [])
 
     function visibilityCallback(index: number) {
         return (isVisible: boolean) => {
@@ -27,7 +47,7 @@ function Musee() {
         }
     }
 
-    const steps = docs.steps
+    const steps = art.steps
         .map((step, i) => {
             const key = `step_${i}`;
             return <MuseeStep key={key} visibilityChanged={visibilityCallback(i)}>
@@ -42,10 +62,13 @@ function Musee() {
             }
         `}</style>
         <div className={styles.scrollingControl}>
-            {steps}
+            { error ? <Error reason={error} /> : steps }
         </div>
         <div className={styles.content}>
-            <MuseeContent index={currentStep} content={docs.steps[currentStep].content} />
+            {
+                art.steps.length > 0 && 
+                <MuseeContent index={currentStep} content={art.steps[currentStep].content} />
+            }
         </div>
     </div>
 }
